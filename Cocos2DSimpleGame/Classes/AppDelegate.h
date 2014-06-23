ee997b0c0e3421be69c5c2c0a2b74f1c063482ae
Jesus Magana <maganazero5@gmail.com>
2014-06-22 22:31:43 -0700

    Initial Commit

---
+++ Cocos2DSimpleGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Cocos2DSimpleGame
+//
+//  Created by Jesus Magana on 6/22/14.
+//  Copyright ZeroLinux5 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Cocos2DSimpleGame/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Jesus Magana on 6/22/14.
+//  Copyright ZeroLinux5 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
